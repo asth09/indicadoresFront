3c93d2d0821089d2514468c20fb5905ba7573981
asth09 <asthackerman@gmail.com>
2026-07-22 15:06:46 -0400

    config

--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
-// config.js
+// client/src/config.js
 export const API_URL = import.meta.env.VITE_API_URL || 
-  (import.meta.env.MODE === 'production' ? '/api' : 'http://localhost:3000/api');
+  (import.meta.env.MODE === 'production' 
+    ? 'https://indicadores-back.vercel.app/api' 
+    : 'http://localhost:3000/api');
