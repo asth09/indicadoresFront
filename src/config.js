eebea3b1ec1716c58f23104303c954d4af585a40
asth09 <asthackerman@gmail.com>
2026-07-22 13:30:28 -0400

    axios

--- src/config.js
+++ src/config.js
@@ -1,1 +1,3 @@
-export const API_URL = import.meta.env.VITE_API_URL|| "/api";
+// config.js
+export const API_URL = import.meta.env.VITE_API_URL || 
+  (import.meta.env.MODE === 'production' ? '/api' : 'http://localhost:3000/api');
